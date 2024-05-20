b151ab656a086e169efd157db0ff8802873c54e4
pacianoseth <feintimg@gmail.com>
2024-05-20 23:55:23 +0800

    feature/added user_profile controller and add profile picture in devise edit and new

---
+++ app/controllers/user_profile_controller.rb
@@ -0,0 +1,6 @@
+class UserProfileController < ApplicationController
+
+    def show
+        @profile = User.find(params[:id])
+    end 
+end
--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
       mount Sidekiq::Web => '/sidekiq'
     end
   resources :posts
-
+  resources :user_profile
   devise_for :users
   root to: 'posts#index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
