5d84224e72d77829bbc1b077e8dbcfa373fee0dd
pacianoseth <feintimg@gmail.com>
2024-06-05 23:07:07 +0800

    rm application=Application.start()

--- app/javascript/controllers/application.js
+++ app/javascript/controllers/application.js
@@ -4,7 +4,6 @@ const application = Application.start()
 // Configure Stimulus development experience
 application.debug = false
 window.Stimulus   = application
-const application = Application.start()
 const context = require.context("controllers", true, /\.js$/)
 application.load(definitionsFromContext(context))
 export { application }
